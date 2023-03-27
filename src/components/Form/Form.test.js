78b14d3fada851f8e245701e364af32d281e3d87
jsan <ahsan.wtc@gmail.com>
2023-03-27 17:11:45 +0200

    adding couple of more tests

--- src/components/Form/Form.test.js
+++ src/components/Form/Form.test.js
@@ -1,4 +1,6 @@
-import { createFormStateObject } from './';
+import { render, screen, fireEvent } from '@testing-library/react';
+
+import Form, { createFormStateObject } from './';
 
 describe('Form Component', () => {
   describe('createFormStateObject', () => {
@@ -22,4 +24,51 @@ describe('Form Component', () => {
     });
 
   });
+
+  describe('interactions', () => {
+
+    let props = {};
+    const onSubmitHandler = jest.fn();
+
+    beforeEach(() => {
+      const attributes = [
+        {
+          label: 'Email',
+          name: 'email',
+          type: 'text',
+        },
+        {
+          label: 'Password',
+          name: 'password',
+          type: 'password',
+        },
+      ];
+      
+      props = { title: 'Title', button: 'button', onSubmit: onSubmitHandler, attributes };
+    });
+
+    it('renders correctly', () => {
+      
+      render (
+        <Form {...props} />
+      );
+      
+      expect(screen.getByText('Title')).toBeInTheDocument();
+      expect(screen.getByText('button')).toBeInTheDocument();
+
+    });
+
+    it('should call handler when submitted', async () => {
+      render (
+        <Form {...props} />
+      );
+      const button = screen.getByRole('button');
+
+      await fireEvent.click(button);
+      expect(onSubmitHandler.mock.calls).toHaveLength(1);
+    });
+
+
+  });
+
 });
